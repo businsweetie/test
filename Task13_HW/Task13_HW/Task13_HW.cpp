f0b6c355eb5e0a7e8828eef216a595c3ef86dc95
businsweetie <Bugashka17@inbox.ru>
2023-04-06 20:38:26 +0700

    commit 2

--- Task13_HW/Task13_HW/Task13_HW.cpp
+++ Task13_HW/Task13_HW/Task13_HW.cpp
@@ -7,21 +7,16 @@
 
 using namespace std;
 
-int MathExp(int* arr, int N) {
-    int M = 0;
-    for (int i = 0; i < N; i++)
-    {
-        M += arr[i];
-    }
-    return M;
-}
-
 double Criteria(int* arr1, int* arr2, int N) {
-    int M1, M2;
+    int M1 = 0;
+    int M2 = 0;
     double max_betw_M1_M2, criteria_value;
 
-    M1 = MathExp(arr1, N);
-    M2 = MathExp(arr2, N);
+    for (int i = 0; i < N; i++)
+    {
+        M1 += arr1[i];
+        M2 += arr2[i];
+    }
 
     max_betw_M1_M2 = max(M1, M2);
     criteria_value = abs((M1 / max_betw_M1_M2) - (M2 / max_betw_M1_M2));
